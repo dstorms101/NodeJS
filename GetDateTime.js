da0454392c6be6a92693dc2f9ae8442031979d0d
dstorms101 <48081352+dstorms101@users.noreply.github.com>
2019-02-28 02:08:42 -0800

    Add files via upload

--- GetDateTime.js
+++ GetDateTime.js
@@ -1,5 +1,5 @@
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds();
 var dateTime = date+' '+time;
 console.log(dateTime);
